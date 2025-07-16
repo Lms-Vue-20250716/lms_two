edf3d5dc0f0b876425b0ac673eeecf7469259959
dydwnsl02 <dydwnsl02@naver.com>
2025-07-16 15:51:01 +0900

    feat: 학생관리 페이지네이션, 목록 조회 기능 구현

---
+++ src/components/Manage/Student/StudentSearch/StudentSearch.vue
@@ -0,0 +1,59 @@
+<script setup>
+import router from '@/router';
+import { onMounted, ref } from 'vue';
+
+
+const searchName = ref('');
+const searchStatysYn = ref('');
+const regStDate = ref('');
+const regEdDate = ref('');
+
+// 검색 버튼을 클릭할 때, 검색 데이터가 QueryParam에 들어가도록 하는 함수
+const handlerSearch = () => {
+  const query = [];
+
+  // 1. searchTitle의 값이 있을 경우, 쿼리라는 array에 담아둠
+  !searchName.value || query.push(`name=${searchName.value}`);
+  !searchStatysYn.value || query.push(`status=${searchStatysYn.value}`);
+  !regStDate.value || query.push(`startDate=${regStDate.value}`);
+  !regEdDate.value || query.push(`endDate=${regEdDate.value}`);
+
+  const queryString = query.length > 0 ? `?${query.join('$')}` : '';
+
+  router.push(queryString);
+};
+
+onMounted(() => {
+  window.location.search && router.replace(window.location.pathname);
+});
+
+</script>
+
+
+
+<template>
+  <div class="student-container">
+    <div class="input-box">
+      이름: <input v-model.lazy="searchName" />
+      재학 상태: <select v-model="searchStatysYn">
+        <option value selected="selected"> 선택 </option>
+        <option value="W"> 승인대기 </option>
+        <option value="Y"> 재학 </option>
+        <option value="N"> 탈퇴 </option>
+      </select>
+      가입 기간: <input v-model="regStDate" type="date" />
+      ~ <input v-model="regEdDate" type="date" />
+      <button @click="handlerSearch">검색</button>
+    </div>
+  </div>
+</template>
+
+<!-- style scoped는 해당 컴포넌트에서만 스타일을 적용시키는 방법이다.
+    그러나, scoped는 컴파일 시점에 고유한 속성을 컴포넌트 요소에 추가하는데,
+    외부 css 파일은 이 고유 속성을 알지 못하므로 스타일이 적용안된다.
+
+    scoped을 사용하지 않고, css파일을 import 하거나, <style scoped> 안에 css를 구성해야한다.
+-->
+<style>
+@import './styled.css';
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Login from '@/views/Auth/Login.vue';
 import DashBoard from '@/views/DashBoard/DashBoard.vue';
+import Student from '@/views/Manage/Student.vue';
 import NotFound from '@/views/NotFound/NotFound.vue';
 import Notice from '@/views/Support/Notice.vue';
 import { createRouter, createWebHistory } from 'vue-router';
@@ -29,6 +30,17 @@ const router = createRouter({
               path: 'notice',
               name: 'notice',
               component: Notice,
+            }
+          ]
+        },
+        {
+          path: 'manage',
+          name: 'manage',
+          children: [
+            {
+              path: 'student',
+              name: 'student',
+              component: Student,
             }
           ]
         }
