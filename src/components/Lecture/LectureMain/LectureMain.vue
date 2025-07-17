e089731d6c5a991ced215f6678f3838eb820b367
Doohwancho <doohwancho526@gmail.com>
2025-07-17 10:01:56 +0900

    feat: 강의목록 pagination

--- src/components/Lecture/LectureMain/LectureMain.vue
+++ src/components/Lecture/LectureMain/LectureMain.vue
@@ -1,5 +1,4 @@
 <script setup>
-import PageNavigation from '@/components/common/PageNavigation.vue';
 import { ref } from 'vue';
 import { useRoute } from 'vue-router';
 import LectureSearch from '../LectureSearch/LectureSearch.vue';
@@ -7,6 +6,7 @@ import { useRouter } from 'vue-router';
 import { watch } from 'vue';
 import { onMounted } from 'vue';
 import axios from 'axios';
+import PageNavigation from '@/components/common/PageNavigation.vue';
 
 //router
 const route = useRoute();
@@ -16,39 +16,40 @@ const router = useRouter();
 const lectureList = ref([]);
 const lectureCount = ref(0);
 
-//필요 파라미터
-//1. pagination: currentPage, pageSize
-//2. searchTitle, searchTag, searchStDate, searchEdDate
-//받는 필드 리스트
-//response.data.usrMnuAtrt = Array()
-//...안에 grp_num / hir_mnu_id / lvl / mnu_dvs_cod / mnu_ico_cod / mnu_id / mnu_nm / mnu_url
-//테이블에 쓸 필드들
-//강의명 / 강사 / 강의시작일 / 강의종료일 / 정원 / 신청인원 / 강의실
+/**
+ * 서버에서 강의 목록 데이터를 비동기적으로 가져옵니다.
+ * 현재 URL의 쿼리 파라미터를 사용하여 필터링 및 페이지네이션을 처리하고,
+ * 그 결과를 컴포넌트의 상태(state)에 반영합니다.
+ */
 const fetchLectureList = async () => {
   try {
     //1. url값 받아다가 유효성검사
     let page = Number(route.query.page || 1);
     let itemPerPage = 5;
 
-    console.log('-------------------');
-    console.log(route.query);
-
+    //2. 강의목록 fetch에 필요한 파라미터를 url에서 가져온다.
+    //필요 파라미터
+    //1. pagination: currentPage, pageSize
+    //2. searchTitle, searchTag, searchStDate, searchEdDate
     const params = new URLSearchParams({
       ...route.query,
       currentPage: page,
       pageSize: itemPerPage,
     });
-    //유효성 검사(null, undefined, "")
+
+    //3. 유효성 검사(null, undefined, "")
     for (let [key, value] of params.entries()) {
       if (!value) {
         params.delete(key);
       }
     }
 
-    //2. axios.get
+    //4. axios.get
     const response = await axios.get(`/api/lecture/lectureListBody.do?${params.toString()}`);
 
     if (response && response.data) {
+      //테이블에 쓸 필드들
+      //강의명 / 강사 / 강의시작일 / 강의종료일 / 정원 / 신청인원 / 강의실
       lectureList.value = response.data.list || [];
       lectureCount.value = response.data.count || 0;
 
@@ -122,11 +123,9 @@ onMounted(() => {
     <PageNavigation
       :total-items="lectureCount"
       :items-per-page="5"
-      :on-page-change="lectureSearch"
+      :on-page-change="fetchLectureList"
     />
   </div>
-  <!-- table  -->
-  <!-- pagination  -->
 </template>
 <style scoped>
 @import './styled.css';
