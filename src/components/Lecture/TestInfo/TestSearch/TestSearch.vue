ef702b877d4122a521d0ac1a3019b5b63784ecd8
Doohwancho <doohwancho526@gmail.com>
2025-07-21 09:33:25 +0900

    feat: 학습관리_시험정보 - searchbar

--- src/components/Lecture/TestInfo/TestSearch/TestSearch.vue
+++ src/components/Lecture/TestInfo/TestSearch/TestSearch.vue
@@ -3,6 +3,7 @@ import router from '@/router';
 import { useModalState } from '@/stores/modalState';
 import { onMounted, ref } from 'vue';
 
+const searchTag = ref('lecName'); //lecName, lecInstructorName, lecRoomName
 const searchTitle = ref('');
 const searchStDate = ref('');
 const searchEdDate = ref('');
@@ -10,9 +11,24 @@ const modalState = useModalState();
 
 // 검색 버튼을 클릭을 할때, 검색 데이터가 queryParam에 들어가게끔 하는 함수
 const handlerSearch = () => {
+  if (searchTag.value == '' || searchTag.value == undefined || searchTag.value == null) {
+    alert('검색 옵션을 넣으세요!');
+    return;
+  }
+  if (searchStDate.value && searchEdDate.value) {
+    const from = new Date(searchStDate);
+    const to = new Date(searchEdDate);
+
+    if (to < from) {
+      alert('종료일이 시작일 보다 더 빠를 수 없습니다!');
+      return;
+    }
+  }
+
   const query = [];
 
   // 1. searchTitle의 값이 있을 경우, 쿼리라는 array에 담아 둘껍니다.
+  !searchTag.value || query.push(`searchTag=${searchTag.value}`);
   !searchTitle.value || query.push(`title=${searchTitle.value}`);
   !searchStDate.value || query.push(`startDate=${searchStDate.value}`);
   !searchEdDate.value || query.push(`endDate=${searchEdDate.value}`);
@@ -30,6 +46,11 @@ onMounted(() => {
 <template>
   <div class="test-container">
     <div class="input-box">
+      <select v-model="searchTag">
+        <option value="lecName">강의명</option>
+        <option value="lecInstructorName">강사명</option>
+        <option value="lecRoomName">강의실</option>
+      </select>
       제목: <input v-model.lazy="searchTitle" />
       <input v-model="searchStDate" type="date" />
       <input v-model="searchEdDate" type="date" />
