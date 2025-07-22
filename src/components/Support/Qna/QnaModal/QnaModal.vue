f199d7caa4f927e594dbc87cca91a41fffe180c2
compactspace <younggoo7000@naver.com>
2025-07-22 17:36:35 +0900

    feat: qna 모달 select는 됨. 등록기능구현중

--- src/components/Support/Qna/QnaModal/QnaModal.vue
+++ src/components/Support/Qna/QnaModal/QnaModal.vue
@@ -98,7 +98,6 @@ const fetchDetail = async () => {
 
   const param = new URLSearchParams();
   param.append('qnaId', props.detailId);
-
   const res = await axios.post('/api/support/getQnaDetail.do', param);
 
   if (res.data) {
