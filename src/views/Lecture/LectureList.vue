002c1dd2142e3867b30a63edb10f3a67df8f9f41
Doohwancho <Doohwancho526@gmail.com>
2025-07-23 09:50:55 +0900

    feat: 학습관리_강의목록 redo (#36)

--- src/views/Lecture/LectureList.vue
+++ src/views/Lecture/LectureList.vue
@@ -1,6 +1,10 @@
 <script setup>
-import LectureMain from '@/components/Lecture/LectureList/LectureMain/LectureMain.vue';
+import ContentBox from '@/components/common/ContentBox.vue';
+import LectureMain from '@/components/Lecture/LectureList/LectureMain_/LectureMain.vue';
+import LectureSearch from '@/components/Lecture/LectureList/LectureSearch_/LectureSearch.vue';
 </script>
 <template>
+  <ContentBox>강의 목록</ContentBox>
+  <LectureSearch />
   <LectureMain />
 </template>
