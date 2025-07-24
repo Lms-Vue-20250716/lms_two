b41dab552eac385c987e9fe36c7baaca4d676bba
Keonhwi99 <keonhwi99>
2025-07-24 12:30:33 +0900

    css 개선

--- src/components/system/Equipment/EquipmentMain/EquipmentMain.vue
+++ src/components/system/Equipment/EquipmentMain/EquipmentMain.vue
@@ -97,7 +97,7 @@ onMounted(() => {
             <span v-if="equip.remainPeroid >= 0">{{ equip.remainPeroid }}일</span>
             <span
               v-else
-              class="equipment-cell cursor-pointer text-red-600 underline"
+              class="equipment-cell cursor-pointer !font-bold !text-red-600 underline"
               @click="deleteEquip(equip.equipId)"
               >장비 삭제</span
             >
