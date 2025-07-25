0a4900124e5e23fc272c0220d150dd3c8ea0459c
Arielchae <cothgud@gmail.com>
2025-07-25 14:45:01 +0900

    feat: qna 모달 진행중

--- src/components/Support/Qna/QnaModal/QnaModal.vue
+++ src/components/Support/Qna/QnaModal/QnaModal.vue
@@ -237,6 +237,7 @@ onUnmounted(() => {
                 </option>
               </select>
               <span>{{ detail.lecName }}</span>
+              ======= >>>>>>> 398b565 (feat: qna 모달 진행중)
             </div>
             <div class="form-row">
               <label>제목:</label>
