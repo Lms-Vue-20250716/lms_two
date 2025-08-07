df848d23e00113d454f1f0fd239ae347744b6ff2
Arielchae <cothgud@gmail.com>
2025-08-07 12:44:42 +0900

    fix

--- src/views/Support/Qna.vue
+++ src/views/Support/Qna.vue
@@ -1,19 +1,11 @@
 <script setup>
 import ContentBox from '@/components/common/ContentBox.vue';
 import QnaMain from '@/components/Support/Qna/QnaMain/QnaMain.vue';
-<<<<<<< HEAD
 // import QnaSearch from '@/components/Support/Qna/QnaSearch/QnaSearch.vue';
-=======
-import QnaSearch from '@/components/Support/Qna/QnaSearch/QnaSearch.vue';
->>>>>>> 6806e40 (학습지원: Qna 진행중)
 </script>
 
 <template>
   <ContentBox> Q&A</ContentBox>
-<<<<<<< HEAD
   <!-- <QnaSearch /> -->
-=======
-  <QnaSearch />
->>>>>>> 6806e40 (학습지원: Qna 진행중)
   <QnaMain />
 </template>
